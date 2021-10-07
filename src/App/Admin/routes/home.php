a93cd9a3425cc4ac67b5b3e14a70cfee7761c0d5
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-10-07 16:13:46 +0500

    feat: добавить роуты для департаментов

--- src/App/Admin/routes/home.php
+++ src/App/Admin/routes/home.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+use App\App\Admin\Controllers\DepartmentsController;
+use App\App\Admin\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\App\Admin\Controllers\HomeController::class, 'index']);
+Route::get('/users', [HomeController::class, 'users']);
+
+Route::prefix('departments')->group(function () {
+    Route::get('/',[DepartmentsController::class,'index']);
+    Route::get('/create',[DepartmentsController::class,'create'])->name('create');
+    Route::post('/store',[DepartmentsController::class,'store'])->name('store');
+});
