bf66a1e4ef6b41b79a2c9f883ec79044cb06ee2a
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-10-08 15:17:42 +0500

    feat: добавить фабрику для модели департамент

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Domain\Department\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+
+class DepartmentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Department::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->title(),
+            'image_path'=>$this->faker->url
+        ];
+    }
+
+    protected static function newFactory()
+    {
+        return FlightFactory::new();
+    }
+
+
+}
