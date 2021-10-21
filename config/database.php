7ccc57394fd2578279577f77dbc7d51d13a4e617
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-10-21 19:35:10 +0500

    refactor: изменить подключение к бд для проведение тестов

--- config/database.php
+++ config/database.php
@@ -40,6 +40,11 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        'sqlite_test' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE'),
+            'prefix' => '',
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
@@ -119,7 +124,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
