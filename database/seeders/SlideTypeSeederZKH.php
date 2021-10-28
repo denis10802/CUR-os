5c4f286e6df6b78cfcc4335c4d18cf4ebc16a88d
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-10-28 13:46:54 +0500

    fix(slideTypes): исправить ошибки

--- database/seeders/SlideTypeSeederZKH.php
+++ database/seeders/SlideTypeSeederZKH.php
@@ -123,7 +123,7 @@ class SlideTypeSeederZKH extends Seeder
             'typeOfWork' => 'Вид работ',
             'image' => 'Изображение',
         ];
-        $slide->sazkh
+        $slide->save()
         $slide = new SlideTypes();
         $slide->image_path = Storage::path('zkh/zkh_10.jpg');
         $slide->field_maps_json = [
